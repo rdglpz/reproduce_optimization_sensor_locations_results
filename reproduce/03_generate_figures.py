4b2d6a98c33e0f693143efcfd9a0548eb604a8d8
Rodrigo Lopez-Farias <rdglpz@gmail.com>
2023-03-09 15:28:13 -0600

    new .py files to reproduce the experiment

---
+++ reproduce/03_generate_figures.py
@@ -0,0 +1,183 @@
+#Cargando librerías 
+import importlib
+
+import matplotlib.pyplot as plt
+
+from matplotlib import cm
+import matplotlib as mpl
+import itertools as it
+
+
+#from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+
+import scipy as sp
+import numpy as np
+import pandas as pd
+import os
+import sys  
+
+root = os.getcwd() + "/.."
+sys.path.insert(0, root)
+
+import src.positioning_sensors as ps
+import src.regionGrowing as rg
+import src.manageExperiments as me
+import src.network_fitness as netfit
+
+importlib.reload(ps)
+importlib.reload(rg)
+importlib.reload(me)
+importlib.reload(netfit)
+
+from geneticalgorithm import geneticalgorithm as ga
+
+
+
+import config.config as cfg
+importlib.reload(cfg)
+
+
+fn = "config_exp_15062022_redo_exp.txt"
+setup = me.readConfigFile(fn)
+
+
+#Loading precalculated local and directed empirical variograms
+
+filesv = (cfg.experiment + setup["experiment_id"] 
+          + setup["folder_semivariances"] 
+          + setup["output_semivariances"] 
+          + ".csv"
+         )
+
+filesvmask = (cfg.experiment + setup["experiment_id"] 
+              + setup["folder_semivariances"] 
+              + setup["output_semivariances"] 
+              + "mask.csv"
+             )
+
+OptimumValues = (cfg.experiment + setup["experiment_id"] 
+                 + setup["folder_output"] 
+                 + setup["output_values"]
+                )
+
+arguments = (cfg.experiment + setup["experiment_id"] 
+             + setup["folder_output"] 
+             + setup["output_args"]
+            )
+
+
+data = pd.read_csv(filesv)
+data_m = pd.read_csv(filesvmask)
+variogram_set = np.array(data.iloc[:,3:])
+variogram_set_m = np.array(data_m.iloc[:,3:])
+
+#cargamos imagenes en luminance e importance
+ilumina = setup["folder_input"] + setup["input_ntli"]
+niveles = setup["folder_input"] + setup["input_evm"]
+sigma_y = setup['filterg_sy']
+sigma_x = setup['filterg_sx']
+sigma = [sigma_y, sigma_x]
+
+
+#NLTI: luminance
+luminance = ps.readIMG(ilumina)
+
+#EAM: Environtmental attention map is the importance
+EAM = ps.readIMG(niveles, invert = True)
+
+DNTLI, b = ps.desaturate(luminance, th = 63)
+
+variograms = variogram_set.reshape(len(variogram_set), 
+                                   DNTLI.shape[0], 
+                                   DNTLI.shape[1]
+                                  )
+variograms_m = variogram_set_m.reshape(len(variogram_set), 
+                                       DNTLI.shape[0],DNTLI.shape[1])
+
+coords = np.array(data.iloc[:, 1:3])
+
+FDNTLI = sp.ndimage.gaussian_filter(DNTLI, sigma, 
+                                    mode = setup['gaussian_mode']
+                                   )
+
+FDNTLI = (FDNTLI >= setup['neglect_values'])*FDNTLI
+
+sensitivity = ps.f5(FDNTLI, EAM, 64)
+
+#cargamos imagenes en luminance e importance
+ilumina = setup["folder_input"] + setup["input_ntli"]
+niveles = setup["folder_input"] + setup["input_evm"]
+sigma_y = setup['filterg_sy']
+sigma_x = setup['filterg_sx']
+sigma = [sigma_y,sigma_x]
+
+aptitude = netfit.NetworkFitness(FDNTLI, EAM, sensitivity, variograms, variograms_m, coords)
+aptitude.selectFitnessFunction("max")
+
+#res_df= pd.DataFrame(results)
+#res_df.to_csv("results_7x7.csv")
+res_read = pd.read_csv(OptimumValues)
+rr = np.array(res_read)
+r = list([])
+for i,j in enumerate(rr):
+    r.append(j[1:(1)+((i % 7) +1)*2])
+
+R = (aptitude.project(r[48])+(FDNTLI>0)*1).astype(int)
+
+def plotSolutions(p,img):
+
+    z = np.copy(img)
+    cs = int(9)
+
+    a = p.reshape(-1,2)
+    for b in a:
+        c = b.astype(int)
+        z[tuple(c)] = 2
+        z[tuple([c[0]+1,c[1]])]=cs
+        z[tuple([c[0]+1,c[1]+1])]=cs
+        z[tuple([c[0],c[1]+1])]=cs
+        z[tuple([c[0]-1,c[1]-1])]=cs
+        z[tuple([c[0]-1,c[1]])]=cs
+        z[tuple([c[0]-1,c[1]+1])]=cs
+        z[tuple([c[0],c[1]-1])]=cs
+        z[tuple([c[0]+1,c[1]-1])]=cs
+        
+
+    return z
+
+P = plotSolutions(r[2],R)
+
+
+R = plotSolutions(r[48], (FDNTLI>0)*1).astype(int)   
+
+
+fig, axs = plt.subplots(ncols=7, nrows=7, figsize=(15, 15),constrained_layout=True)
+
+# add an artist, in this case a nice label in the middle...
+ix = 0
+saveR = np.zeros(R.shape)
+f = 0
+c = setup["sensitivity_c"]
+for row in range(len(c)):
+    for col in range(7):
+        R = (aptitude.project(r[ix])+(FDNTLI>0)*1).astype(int)
+        PS = plotSolutions(r[ix],R)
+     #   R = plotSolutions(r[ix],(FDNTLI>0)*1).astype(int)
+    #    R = plotSolutions(r[ix],R)
+        #R_and_locations = AttachLocations(R,results)
+        axs[row,col].imshow(PS,cmap="viridis",interpolation='none')
+
+        if col==0: 
+            axs[row, col].set_ylabel("$c={c: 1.0f}$".format(c = c[row]),rotation = 0,fontsize=18)
+            axs[row, col].yaxis.set_label_coords(-.27, .5)
+        if row==0: 
+            axs[row, col].set_title('$n = {n: 1.0f}$'.format(n=col+1),fontsize=18)
+            #axs[row, col].set_xlabel("n = {n: 1.0f}".format(n=col+1))
+            
+        axs[row, col].tick_params(left = False, right = False , labelleft = False , labelbottom = False, bottom = False)
+
+        ix += 1
+
+
+plt.savefig("covers64_2.pdf",format='pdf', dpi=500,bbox_inches="tight")
+
